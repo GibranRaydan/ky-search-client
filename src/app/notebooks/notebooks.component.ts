f4a203cbed3071c8040acb220bbf43dd63ceb8e6
Gibran <gibran.r@courthousecomputersystems.com>
2024-07-11 16:37:48 -0500

    Feature, change basic layout of seacrh component

--- src/app/notebooks/notebooks.component.ts
+++ src/app/notebooks/notebooks.component.ts
@@ -1,28 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, AfterViewInit, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-notebooks-table',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatProgressSpinnerModule],
+  imports: [CommonModule, MatTableModule, MatProgressSpinnerModule, MatPaginatorModule],
   templateUrl: './notebooks.component.html',
   styleUrls: ['./notebooks.component.css']
 })
-export class NotebooksTableComponent {
+export class NotebooksTableComponent implements OnInit, AfterViewInit, OnChanges {
   @Input() notebooks: any[] = [];
   @Input() loading: boolean = false;
-  displayedColumns: string[] = ['grantor', 'grantee', 'kind', 'book', 'page', 'date']; // Add more column names as needed
-  //displayedColumns: string[] = [
-  //  'image', 'filler', 'code', 'grantorA', 'grantor', 'grantorGiven1',
-  //  'grantorGiven2', 'gorAp', 'granteeA', 'grantee', 'granteeGiven1',
-  //  'granteeGiven2', 'geeAP', 'kind', 'book', 'page', 'numberPages',
-  //  'lastPage', 'desc', 'docNo', 'date', 'time', 'alt', 'blank', 'money',
-  //  'idX1', 'idX2', 'idX3', 'idX4', 'idX5', 'idX6', 'idX7', 'idX8', 'idX9',
-  //  'idX10', 'recordChanged', 'refKind', 'refBook', 'refPage', 'isRefParent',
-  //  'isORFirstParty', 'isEEFirstParty', 'grantorStripped', 'grantorGiven1Stripped',
-  //  'grantorGiven2Stripped', 'granteeStripped', 'granteeGiven1Stripped',
-  //  'granteeGiven2Stripped', 'primKey', 'docNumber'
-  //];
+  displayedColumns: string[] = ['grantor', 'grantee', 'kind', 'book', 'page', 'date'];
+
+  dataSource = new MatTableDataSource<any>(this.notebooks);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngOnInit() {
+    this.dataSource.data = this.notebooks;
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['notebooks']) {
+      this.dataSource.data = this.notebooks;
+    }
+  }
 }
